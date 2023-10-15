b0d4423237821b70e6d2dd1918af122b256232a6
czyjtu <maciejczyjtu@gmail.com>
2023-10-15 16:54:07 +0200

    observation is represented as dictionay containing arena descrition and ChampionKnowledge

--- gupb/gym_env/gym_env/__init__.py
+++ gupb/gym_env/gym_env/__init__.py
@@ -1,7 +1,27 @@
 import gym 
 from gym.envs.registration import register
+import gym.spaces 
 
 from gupb import controller
+from gupb.model.characters import ChampionKnowledge
+from gupb.model.coordinates import Coords
+
+class ViewSpace(gym.spaces.Space):
+    # this class is just a boilerplate to make gym happy
+    def __init__(self):
+        super().__init__()
+
+    def sample(self):
+        return None
+
+    def contains(self, x):
+        return isinstance(x, ChampionKnowledge)
+
+    def __repr__(self):
+        return f"ViewSpace"
+
+    def __eq__(self, other):
+        return isinstance(other, ViewSpace)
 
 
 class GUPBEnv(gym.Env):
@@ -19,10 +39,10 @@ class GUPBEnv(gym.Env):
     
     @property
     def observation_space(self):
-        return gym.spaces.Discrete(3)
+        return gym.spaces.Dict({"arena": gym.spaces.Discrete(1), "view": ViewSpace()})
 
     def reset(self):
-        return 1
+        return {"arena": 0, "view": ChampionKnowledge(Coords(0, 0), 0, {})}
 
     def step(self, action):
         pass
--- gupb/gym_env/test/test_gym_interface.py
+++ gupb/gym_env/test/test_gym_interface.py
@@ -1,12 +1,35 @@
+import numpy as np
 import pytest 
 import gym 
 
+from gupb.controller import random
 import gupb.gym_env.gym_env
+from gupb.model.characters import ChampionKnowledge
 
-@pytest.fixture
-def env() -> gym.Env:
-    return gym.make('GUPB-v0')
 
-def test_env_build_successfully(env: gym.Env):
+def test_env_build_successfully():
+    env: gym.Env = gym.make('GUPB-v0')
+
     assert env is not None
     assert env.reset() is not None
+
+
+def test_reset_return_arena_and_view():
+    env: gym.Env = gym.make('GUPB-v0')
+
+    obs = env.reset()
+
+    assert obs is not None
+    match obs:
+        case {"arena": _, "view": _}: 
+            pass
+        case _:
+            assert False, obs
+
+def test_observation_contains_actual_view():
+    env: gym.Env = gym.make('GUPB-v0')
+
+    obs = env.reset()
+
+    assert obs is not None
+    assert isinstance(obs["view"], ChampionKnowledge)
