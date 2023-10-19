02fdc306af392579088582c5c834b04cb21b90e7
czyjtu <maciejczyjtu@gmail.com>
2023-10-19 21:31:28 +0200

    cnn return correct number of batches

--- gupb/controller/noname/policy/cnn.py
+++ gupb/controller/noname/policy/cnn.py
@@ -3,7 +3,7 @@ import torch as th
 class CNNPolicy(th.nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        
-    def forward(self, observation):
-        return th.rand(1, 1, 5)
+
+    def forward(self, X: th.Tensor) -> th.Tensor:
+        return th.rand(X.shape[0], 5)
     
--- gupb/gym_env/test/test_cnn_policy.py
+++ gupb/gym_env/test/test_cnn_policy.py
@@ -8,3 +8,11 @@ def test_cnn_network_returns_tensor():
     result = network(observation)
     assert isinstance(result, th.Tensor)
     
+
+@pytest.mark.parametrize("batches", [1, 32, 64, 128])
+def test_cnn_network_returns_correct_num_of_batches(batches):
+    network = CNNPolicy()
+    observation = th.rand(batches, 3, 10, 10)
+    result = network(observation)
+    assert result.shape[0] == batches
+
