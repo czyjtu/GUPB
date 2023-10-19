4686aab5be7465d79dece98bd1732270c32d5bab
czyjtu <maciejczyjtu@gmail.com>
2023-10-19 21:56:15 +0200

    netowrk is deterministic and number of actions is adjustable

--- gupb/controller/noname/policy/cnn.py
+++ gupb/controller/noname/policy/cnn.py
@@ -1,9 +1,31 @@
 import torch as th 
+import typing as tp
+
+
+def ConvBlockNormalizeDrop(in_channels: int, n_filters: int, activation: tp.Literal["sigmoid", "relu"], p: float, gap: bool=False):
+    activation_fun = {"relu": th.nn.ReLU, "sigmoid": th.nn.Sigmoid}[activation]
+    return th.nn.Sequential(
+        th.nn.Conv2d(in_channels, n_filters, 3, padding="same"),
+        activation_fun(),
+        th.nn.BatchNorm2d(n_filters),
+        th.nn.Conv2d(n_filters, n_filters, 3, padding="same"),
+        activation_fun(),
+        th.nn.BatchNorm2d(n_filters),
+        th.nn.AdaptiveAvgPool2d((1, 1)) if gap else th.nn.MaxPool2d(2),
+        th.nn.Dropout2d(p)
+    )
 
 class CNNPolicy(th.nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, actions_num: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.conv1 = ConvBlockNormalizeDrop(3, 8, "relu", 0.2)
+        self.conv2 = ConvBlockNormalizeDrop(8, 32, "relu", 0.2, gap=True)
+        self.linear = th.nn.Linear(32, actions_num)
 
     def forward(self, X: th.Tensor) -> th.Tensor:
-        return th.rand(X.shape[0], 5)
+        X = self.conv1(X)
+        X = self.conv2(X)
+        X = X.view(X.shape[0], -1)
+        X = self.linear(X)
+        return X
     
--- gupb/gym_env/test/test_cnn_policy.py
+++ gupb/gym_env/test/test_cnn_policy.py
@@ -2,17 +2,33 @@ import pytest
 import torch as th 
 from gupb.controller.noname.policy import CNNPolicy
 
-def test_cnn_network_returns_tensor():
-    network = CNNPolicy()
+@pytest.fixture 
+def network():
+    return CNNPolicy(5).eval()
+
+def test_cnn_network_returns_tensor(network):
     observation = th.rand(1, 3, 10, 10)
     result = network(observation)
     assert isinstance(result, th.Tensor)
     
 
 @pytest.mark.parametrize("batches", [1, 32, 64, 128])
-def test_cnn_network_returns_correct_num_of_batches(batches):
-    network = CNNPolicy()
+def test_cnn_network_returns_correct_num_of_batches(batches, network):
     observation = th.rand(batches, 3, 10, 10)
     result = network(observation)
     assert result.shape[0] == batches
 
+
+@pytest.mark.parametrize("X", [th.rand(1, 3, 10, 10), th.rand(32, 3, 10, 10), th.rand(64, 3, 10, 10), th.rand(128, 3, 10, 10)])
+def test_cnn_returns_same_actions_for_same_inputs_and_different_for_different(X, network):
+    Y = th.rand_like(X)
+    result_X1 = network(X)
+    result_X2 = network(X)
+    result_Y = network(Y)
+    assert th.all(result_X1 == result_X2)
+    assert th.any(result_X1 != result_Y)
+
+@pytest.mark.parametrize("actions_num", [1, 5, 10])
+def test_network_output_can_be_adjusted(actions_num):
+    net = CNNPolicy(actions_num=actions_num)
+    assert net(th.rand(1, 3, 10, 10)).shape[1] == actions_num
